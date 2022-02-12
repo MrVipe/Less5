6af7869580d6f487bab2d4dcefa484ee9a531950
MrVipe <mr.vi@mail.ru>
2022-02-12 23:10:27 +0400

    Задание4

--- Less5/Program.cs
+++ Less5/Program.cs
@@ -10,11 +10,33 @@ namespace Less5
     internal class Program
     {
         static void Main(string[] args)
-        {
-            File.WriteAllText("newtext.txt", Console.ReadLine());
+        {   
+            File.WriteAllText("recurDir.txt", ""); //чистим
+            string path = @"C:\Users\lena\source\repos\test\test";
 
-            Console.WriteLine(File.ReadAllText("newtext.txt"));
+            GetNewPath(0, path);
+            
+            Console.WriteLine(File.ReadAllText("recurDir.txt"));
             Console.ReadKey();
         }
+
+        static void GetNewPath(int lvl, string oldPath)
+        {
+            File.AppendAllText("recurDir.txt", oldPath);// Записываем директорию
+            string[] getDir = Directory.GetDirectories(oldPath); //получаем директорию
+            string[] getFile =  Directory.GetFiles(oldPath).Select(Path.GetFileName).ToArray(); //получаем файлы в тек. директории
+            File.AppendAllLines("recurDir.txt", getFile); //записываем файлы
+
+            if (getDir.Length != 0)
+            {
+                foreach (string item in getDir) //проходы по директориям
+                {
+                    GetNewPath(lvl + 1, item);
+                }
+            }
+        }
+
     }
+
 }
+
